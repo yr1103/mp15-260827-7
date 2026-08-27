36dcb351a62d8b13717141b7de3541d1e931e057
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-27 17:46:08 +0900

    [Feat] 본인 소개 메서드 추가
    Closes #2

--- 260827_secondGroupTask/260827_secondGroupTask/Program.cs
+++ 260827_secondGroupTask/260827_secondGroupTask/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program  
+{  
+    public static void Main()  
+    {  
+        Console.WriteLine("=== N조 팀원 소개 ===");  
+        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        IntroSwchoiDev();
+
+        Console.WriteLine("=== 소개 끝 ===");  
+    }
+
+    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void IntroSwchoiDev()
+    {
+        Console.WriteLine("최성원 / 진짜 고양이들을 키웁니다");  
+    }
+}
